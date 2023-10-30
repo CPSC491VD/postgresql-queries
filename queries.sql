bc0c31272e9471c95175ae4bfb97bd101368164c
Daniel Cazarez <danc20182@gmail.com>
2023-10-30 12:57:16 -0700

    add queries

---
+++ queries.sql
@@ -0,0 +1,32 @@
+-- Query to create destination table (to store results from API for now)
+CREATE TABLE crime_data (
+    crime_date DATE,
+    district VARCHAR(3),
+    block VARCHAR(50),
+    description VARCHAR(100),
+    location_description VARCHAR(100),
+    updated_on DATE,
+    community_area INT,
+    iucr VARCHAR(4),
+    ward INT,
+    case_number VARCHAR(10),
+    year_of INT,
+    domestic BOOLEAN,
+    fbi_code VARCHAR(3),
+    beat INT,
+    primary_type VARCHAR(50),
+    arrest BOOLEAN,
+    id SERIAL PRIMARY KEY,
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    insert_date DATE
+);
+
+-- Query to hold IUCR data (will be used later for creating fact, dimension tables)
+CREATE TABLE iucr (
+    iucr VARCHAR(255) UNIQUE NOT NULL,
+    primary_description VARCHAR(255) NOT NULL,
+    secondary_description VARCHAR(255) NOT NULL,
+    index_code VARCHAR(255) NOT NULL,
+    active BOOLEAN NOT NULL
+);
