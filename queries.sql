bb6b88f3c457a912a817fe4ee66b717c779a9d36
Vu Diep <54611122+vudiep411@users.noreply.github.com>
2023-11-06 14:36:01 -0800

    Update IF NOT EXISTS, fbi_code queries.sql

--- queries.sql
+++ queries.sql
@@ -1,29 +1,29 @@
--- Query to create destination table (to store results from API for now)
-CREATE TABLE crime_data (
-    crime_date DATE,
-    district VARCHAR(3),
-    block VARCHAR(50),
-    description VARCHAR(100),
-    location_description VARCHAR(100),
-    updated_on DATE,
-    community_area INT,
-    iucr VARCHAR(4),
-    ward INT,
-    case_number VARCHAR(10),
-    year_of INT,
-    domestic BOOLEAN,
-    fbi_code VARCHAR(3),
-    beat INT,
-    primary_type VARCHAR(50),
-    arrest BOOLEAN,
-    id SERIAL PRIMARY KEY,
-    latitude DOUBLE PRECISION,
-    longitude DOUBLE PRECISION,
-    insert_date DATE
-);
+-- -- Query to create destination table (to store results from API for now)
+-- CREATE TABLE crime_data (
+--     crime_date DATE,
+--     district VARCHAR(3),
+--     block VARCHAR(50),
+--     description VARCHAR(100),
+--     location_description VARCHAR(100),
+--     updated_on DATE,
+--     community_area INT,
+--     iucr VARCHAR(4),
+--     ward INT,
+--     case_number VARCHAR(10),
+--     year_of INT,
+--     domestic BOOLEAN,
+--     fbi_code VARCHAR(3),
+--     beat INT,
+--     primary_type VARCHAR(50),
+--     arrest BOOLEAN,
+--     id SERIAL PRIMARY KEY,
+--     latitude DOUBLE PRECISION,
+--     longitude DOUBLE PRECISION,
+--     insert_date DATE
+-- );
 
 -- Query to hold IUCR data (will be used later for creating fact, dimension tables)
-CREATE TABLE iucr (
+CREATE TABLE IF NOT EXISTS iucr (
     iucr VARCHAR(255) UNIQUE NOT NULL,
     primary_description VARCHAR(255) NOT NULL,
     secondary_description VARCHAR(255) NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE iucr (
     active BOOLEAN NOT NULL
 );
 
-CREATE TABLE crime_fact_table (
+CREATE TABLE IF NOT EXISTS crime_fact_table (
 	id VARCHAR,
 	datetime_id INT,
 	description VARCHAR,
@@ -40,11 +40,12 @@ CREATE TABLE crime_fact_table (
 	iucr VARCHAR,
 	case_number VARCHAR,
 	domestic BOOLEAN,
+	fbi_code VARCHAR,
 	primary_type VARCHAR(50),
 	arrest BOOLEAN
 );
 
-CREATE TABLE crime_date_dimension (
+CREATE TABLE IF NOT EXISTS crime_date_dimension (
 	datetime_id INT,
 	_month INT,
 	_day INT,
@@ -53,7 +54,7 @@ CREATE TABLE crime_date_dimension (
 	crime_date DATE
 );
 
-CREATE TABLE crime_location_dimension (
+CREATE TABLE IF NOT EXISTS crime_location_dimension (
 	location_id INT primary key,
 	latitude FLOAT,
 	longitude FLOAT,
