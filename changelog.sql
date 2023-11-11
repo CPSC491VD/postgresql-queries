ded20102376686228a9e12e24317a46ee97055d6
vudiep411 <vudiep411@gmail.com>
2023-11-11 12:14:15 -0800

    fix tbl_analytics table

--- changelog.sql
+++ changelog.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset vu_diep:2
+--changeset vu_diep:3
 CREATE TABLE IF NOT EXISTS iucr (
     iucr VARCHAR(255) UNIQUE NOT NULL,
     primary_description VARCHAR(255) NOT NULL,
@@ -49,18 +49,18 @@ CREATE TABLE IF NOT EXISTS tbl_analytics AS (
 SELECT 
 f.id,
 d.crime_date,
-f.description,
+f.description AS crime_description,
 l.latitude,
 l.longitude,
 l.location_description,
-i.primary_description,
+i.primary_description AS iucr_primary_description,
+i.secondary_description AS iucr_secondary_description,
+i.active AS iucr_active,
 f.case_number,
-fbi.description AS fbi_code_description,
 f.primary_type,
 f.arrest
 FROM crime_fact_table f
 JOIN crime_date_dimension d ON f.datetime_id = d.datetime_id
 JOIN crime_location_dimension l ON l.location_id = f.location_id
-JOIN fbi_code fbi ON fbi.id = f.fbi_code 
-JOIN iucr i ON i.iucr = f.iucr 
+LEFT JOIN iucr i ON i.iucr = f.iucr 
 );
